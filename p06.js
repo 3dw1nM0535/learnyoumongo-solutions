bdcd2588b04a300edbf201c21c55c98290b374e3
shnere <alanrodriguezromero@gmail.com>
2015-07-19 11:43:33 -0500

    Sixth problem

---
+++ p06.js
@@ -0,0 +1,17 @@
+var mongo = require('mongodb').MongoClient
+    mongo.connect('mongodb://localhost:27017/learnyoumongo', function(err, db) {
+        if(err) throw err;
+        var users = db.collection('users');
+
+        docs.update({
+            username: "tinatime"
+        },{
+            $set:{
+                age: 40
+            }
+        }, function(err) {
+            if (err) throw err
+            db.close()
+        });
+    );
+});
